e45c344023c1cf8c26ed751e17e79849b72035fd
Chukwuemeka <emekaojih@gmail.com>
2024-02-05 17:17:59 -0500

    Corrected Formatting errors

--- large_scale_programming_2024/src/org/howard/edu/lsp/assignment2/WarmUp.java
+++ large_scale_programming_2024/src/org/howard/edu/lsp/assignment2/WarmUp.java
@@ -1,6 +1,7 @@
 /**
 * Name: Chukwuemeka Ojih
-*/package org.howard.edu.lsp.assignment2;
+*/
+package org.howard.edu.lsp.assignment2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,26 +11,26 @@ import java.util.Map;
 public class WarmUp {
 
 	public static void main(String[] args) {
-		Map<String, Integer> wordFrequency = new HashMap(); 
+		Map<String, Integer> wordFrequency = new HashMap<>();
 		String line;
-	    String filePath = "src/org/howard/edu/lsp/assignment2/words.txt";
-	    try {
-    		BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
+		String filePath = "src/org/howard/edu/lsp/assignment2/words.txt";
+		try {
+			BufferedReader fileReader = new BufferedReader(new FileReader(filePath));
 			while ((line = fileReader.readLine()) != null) {
 				for (String word: line.split("\\s+")) {
 					word = word.toLowerCase();
 					String[] letterArray = word.split("[^a-z]");
 					for (String subWord: letterArray) {
 						if (subWord.length() > 3) {
-			                wordFrequency.put(subWord, wordFrequency.getOrDefault(subWord, 0) + 1);
-			            }
+							wordFrequency.put(subWord, wordFrequency.getOrDefault(subWord, 0) + 1);
+						}
 					}
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	    wordFrequency.forEach((word, count) -> System.out.println(word + " " + count));
+		wordFrequency.forEach((word, count) -> System.out.println(word + " " + count));
 	};
 
 };
