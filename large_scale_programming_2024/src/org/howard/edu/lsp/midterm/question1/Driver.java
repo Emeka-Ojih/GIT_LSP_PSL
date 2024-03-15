852ca510b8720ee09b92bb6fb5147040d5c3c2c2
Chukwuemeka <emekaojih@gmail.com>
2024-03-14 20:52:54 -0400

    Correction

--- large_scale_programming_2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
+++ large_scale_programming_2024/src/org/howard/edu/lsp/midterm/question1/Driver.java
@@ -7,14 +7,6 @@ public class Driver {
         // Test cases to ensure the encrypt method covers a variety of scenarios
         String[] testStrings = {
             "I love CSCI363",      // Original test case with mixed case and numbers
-            "Hello, World",       // Test case with punctuation
-            "Sphinx of black quartz, judge my vow.",  // Test case with longer string
-            "1234567890",          // Test case with digits only
-            "The quick brown fox jumps over the lazy dog.",  // Test case with all alphabets
-            "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz",  // Test case with alternating cases
-            " ",                   // Test case with spaces only
-            "!@#&*$%^",        // Test case with special characters only
-            ""                      // Test case with empty string
         };
 
         // Loop through all test cases
